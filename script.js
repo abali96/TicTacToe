606e926ea8cd87c31d9357a4df29f9ce28d30112
Arjun <arjun.bali96@gmail.com>
2014-07-08 15:36:46 -0400

    after ryan's fix -- it works but no restart possibility

--- script.js
+++ script.js
@@ -1,53 +1,57 @@
 $(document).ready(function () {
 
-(startGame = function() {
+  var xo = "<p>O</p>";
+  var i = 0;
 
-  var i = 0
-  var xo = 0
-  var victory
   function xoxo() {
     if (i % 2 == 0) {
-      xo = "<p>X</p>";
+      xo = "<p style='color: white;'>X</p>";
       $("#help-text").text("O's turn");
     } else {
-      xo = "<p>O</p>"
+      xo = "<p style='color: white;'>O</p>"
       $("#help-text").text("X's turn");
     }
-    i += 1
+    console.log(i);
+    i++
+
   }
 
-    function win() {
-    victory = true
-
+  function win() {
+    victory = true;
     if ( $(".left").text() === "XXX" || $(".right").text()  === "XXX" ||  $(".center").text() === "XXX") {
-      $("#help-text").text("X wins!")
+      $("#help-text").text("X wins!");
     } else if ($(".bottom").text() === "XXX" || $(".middle").text() === "XXX" || $(".top").text() === "XXX") {
       $("#help-text").text("X wins!")
-    } else if ( $(".left").text() === "OOO" || $(".right").text()  === "OOO" ||  $(".center").text() === "OOO") {
+    } else if ($(".left").text() === "OOO" || $(".right").text()  === "OOO" ||  $(".center").text() === "OOO") {
       $("#help-text").text("O wins!")
     } else if ($(".bottom").text() === "OOO" || $(".middle").text() === "OOO" || $(".top").text() === "OOO") {
       $("#help-text").text("O wins!")
-    } else if ( $("#top-left > p").text() == "X" && $("#bottom-right > p").text() == "X" && $("#middle-center > p").text() == "X" ){
+    } else if ( $("#top-left > p").text() === "X" && $("#bottom-right > p").text() === "X" && $("#middle-center > p").text() === "X" ){
       $("#help-text").text("X wins!")
-    } else if ( $("#top-left > p").text() == "O" && $("#bottom-right > p").text() == "O" && $("#middle-center > p").text() == "O" ){
+    } else if ( $("#top-left > p").text() === "O" && $("#bottom-right > p").text() === "O" && $("#middle-center > p").text() === "O" ){
       $("#help-text").text("O wins!")
-    } else if ( $("#bottom-left > p").text() == "X" && $("#top-right > p").text() == "X" && $("#middle-center > p").text() == "X" ){
+    } else if ( $("#bottom-left > p").text() === "X" && $("#top-right > p").text() === "X" && $("#middle-center > p").text() === "X" ){
       $("#help-text").text("X wins!")
-    } else if ( $("#bottom-left > p").text() == "O" && $("#top-right > p").text() == "O" && $("#middle-center > p").text() == "O" ){
+    } else if ( $("#bottom-left > p").text() === "O" && $("#top-right > p").text() === "O" && $("#middle-center > p").text() === "O" ){
       $("#help-text").text("O wins!")
-    }
-    else {
-      victory = false
+    } else {
+      victory = false;
     }
   }
 
   function play(id) {
     $(id).one("click", function () {
       xoxo();
+      // if (xo == "<p>O</p>") {
+      //   xo = "<p>X</p>"
+      // } else if (xo == "<p>X</p>") {
+      //   xo = "<p>O</p>"
+      // }
       $(xo).appendTo(id);
+      console.log(xo);
       $(this).toggleClass("unfilled filled");
-      win();
-      new_game();
+       win();
+       new_game();
     });
   }
 
@@ -55,25 +59,40 @@ $(document).ready(function () {
     if (victory == false) {
       console.log("Keep playing");
     } else {
-      $("table").attr('id', 'game-over');
-      $("td").removeClass('unfilled');
+      // $("td.unfilled").trigger("click").children().addClass("autofilled");
     }
   }
 
-  play("#top-left")
-  play("#top-center")
-  play("#top-right")
-  play("#middle-left")
-  play("#middle-center")
-  play("#middle-right")
-  play("#bottom-left")
-  play("#bottom-center")
-  play("#bottom-right")
+  function restart() {
+    i = 0;
+    $("td").addClass("unfilled");
+    $("td").removeClass("filled");
+    $("p").empty().remove();
+  }
 
- })();
+  play("#top-left");
+  play("#top-center");
+  play("#top-right");
+  play("#middle-left");
+  play("#middle-center");
+  play("#middle-right");
+  play("#bottom-left");
+  play("#bottom-center");
+  play("#bottom-right");
+
 
   $('button').click(function() {
-    location.reload();
+    restart();
+
+      // $("td.unfilled").trigger("click").children().addClass("autofilled");
+    // $("td").addClass("unfilled");
+    // $("td").removeClass("filled");
+    // $("p").empty().remove();
+    // startGame();
+    // while(1) {
+    //   startGame();
+    // }
+
   });
 
 });
